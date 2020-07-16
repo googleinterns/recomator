54c59b5539ae9bec3ce3281894b08b7e2a319643
Viktoriia Kovalova <vkovalova@google.com>
2020-07-16 19:54:06 +0300

    added error handling && license

--- pkg/automation/listrecommendations.go
+++ pkg/automation/listrecommendations.go
@@ -1,18 +1,37 @@
+/*
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package automation
 
 import (
 	"fmt"
-	"log"
 
 	"google.golang.org/api/compute/v1"
 	"google.golang.org/api/recommender/v1"
 )
 
+// gcloudRecommendation is a type alias for Google Cloud Recommendation recommender.GoogleCloudRecommenderV1Recommendation
+type gcloudRecommendation = recommender.GoogleCloudRecommenderV1Recommendation
+
 // ListRecommendations returns the list of recommendations for specified project, zone, recommender.
 // projects.locations.recommenders.recommendations/list method from Recommender API is used.
-func (s *googleService) ListRecommendations(project string, location string, recommenderID string) []*recommender.GoogleCloudRecommenderV1Recommendation {
+// If the error occurred the returned error is not nil.
+func (s *googleService) ListRecommendations(project string, location string, recommenderID string) ([]*gcloudRecommendation, error) {
 	listCall := s.recommenderService.List(fmt.Sprintf("projects/%s/locations/%s/recommenders/%s", project, location, recommenderID))
-	var recommendations []*recommender.GoogleCloudRecommenderV1Recommendation
+	var recommendations []*gcloudRecommendation
 	addRecommendations := func(response *recommender.GoogleCloudRecommenderV1ListRecommendationsResponse) error {
 		recommendations = append(recommendations, response.Recommendations...)
 		return nil
@@ -20,14 +39,15 @@ func (s *googleService) ListRecommendations(project string, location string, rec
 
 	err := listCall.Pages(s.ctx, addRecommendations)
 	if err != nil {
-		log.Fatal(err)
+		return []*gcloudRecommendation{}, err
 	}
-	return recommendations
+	return recommendations, nil
 }
 
 // ListZonesNames returns list of zone names for the specified project.
 // Uses zones/list method from Compute API.
-func (s *googleService) ListZonesNames(project string) []string {
+// If the error occurred the returned error is not nil.
+func (s *googleService) ListZonesNames(project string) ([]string, error) {
 	listCall := s.zonesService.List(project)
 
 	var zones []string
@@ -39,17 +59,20 @@ func (s *googleService) ListZonesNames(project string) []string {
 	}
 	err := listCall.Pages(s.ctx, addZones)
 	if err != nil {
-		log.Fatal(err)
+		return []string{}, err
 	}
-	return zones
+	return zones, nil
 }
 
 // ListRecommendations returns the list of recommendations for a Cloud project.
 // Requires the recommender.*.list IAM permission for the specified recommender.
-// numConcurrentCalls specifies the maximum number of concurrent calls to ListZoneRecommendations,
+// numConcurrentCalls specifies the maximum number of concurrent calls to ListRecommendations method,
 // non-positive values are ignored, instead the default value is used.
-func ListRecommendations(service GoogleService, project string, recommenderID string, numConcurrentCalls int) []*recommender.GoogleCloudRecommenderV1Recommendation {
-	zones := service.ListZonesNames(project)
+func ListRecommendations(service GoogleService, project string, recommenderID string, numConcurrentCalls int) ([]*gcloudRecommendation, error) {
+	zones, err := service.ListZonesNames(project)
+	if err != nil {
+		return []*gcloudRecommendation{}, err
+	}
 	numberOfZones := len(zones)
 
 	numWorkers := numConcurrentCalls
@@ -58,12 +81,26 @@ func ListRecommendations(service GoogleService, project string, recommenderID st
 		numWorkers = defaultNumWorkers
 	}
 
-	results := make(chan []*recommender.GoogleCloudRecommenderV1Recommendation, numberOfZones)
+	results := make(chan []*gcloudRecommendation, numberOfZones)
 	zonesJobs := make(chan string, numberOfZones)
+	errors := make(chan error)
+	quit := make(chan bool)
+
 	for i := 0; i < numWorkers; i++ {
 		go func() {
 			for zone := range zonesJobs {
-				results <- service.ListRecommendations(project, zone, recommenderID)
+				select {
+				case <-quit:
+					break
+				default:
+					result, err := service.ListRecommendations(project, zone, recommenderID)
+					if err != nil {
+						errors <- err
+						close(quit)
+						break
+					}
+					results <- result
+				}
 			}
 		}()
 	}
@@ -73,9 +110,14 @@ func ListRecommendations(service GoogleService, project string, recommenderID st
 	}
 	close(zonesJobs)
 
-	var recommendations []*recommender.GoogleCloudRecommenderV1Recommendation
+	var recommendations []*gcloudRecommendation
 	for range zones {
-		recommendations = append(recommendations, <-results...)
+		select {
+		case err := <-errors:
+			return []*gcloudRecommendation{}, err
+		case result := <-results:
+			recommendations = append(recommendations, result...)
+		}
 	}
-	return recommendations
+	return recommendations, nil
 }
--- pkg/automation/listrecommendations_test.go
+++ pkg/automation/listrecommendations_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package automation
 
 import (
@@ -7,7 +23,6 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
-	"google.golang.org/api/recommender/v1"
 )
 
 type MockService struct {
@@ -18,27 +33,85 @@ type MockService struct {
 	zonesCalled                           []string
 }
 
-func (s *MockService) ListRecommendations(project string, location string, recommenderID string) []*recommender.GoogleCloudRecommenderV1Recommendation {
+func (s *MockService) ListRecommendations(project string, location string, recommenderID string) ([]*gcloudRecommendation, error) {
 	s.mutex.Lock()
 	s.numberOfTimesListRecommendationsCalls++
 	s.zonesCalled = append(s.zonesCalled, location)
 	s.mutex.Unlock()
-	return []*recommender.GoogleCloudRecommenderV1Recommendation{}
+	return []*gcloudRecommendation{}, nil
 }
 
-func (s *MockService) ListZonesNames(project string) []string {
-	return s.zones
+func (s *MockService) ListZonesNames(project string) ([]string, error) {
+	return s.zones, nil
 }
 
 func TestListRecommendations(t *testing.T) {
 	for numConcurrentCalls := 0; numConcurrentCalls <= 5; numConcurrentCalls++ {
 		zones := []string{"zone1", "zone2", "zone3"}
 		mock := &MockService{zones: zones}
-		result := ListRecommendations(mock, "", "", numConcurrentCalls)
+		result, err := ListRecommendations(mock, "", "", numConcurrentCalls)
 
-		assert.Equal(t, 0, len(result), "No recommedations expected")
-		assert.Equal(t, len(zones), mock.numberOfTimesListRecommendationsCalls, "Wrong number of listZoneRecommendaions calls")
-		assert.ElementsMatch(t, mock.zones, mock.zonesCalled, "listZoneRecommendations was called for different zones")
+		if assert.NoError(t, err, "Unexpected error from ListRecommendations") {
+			assert.Equal(t, 0, len(result), "No recommendations expected")
+			assert.Equal(t, len(zones), mock.numberOfTimesListRecommendationsCalls, "Wrong number of ListRecommendations calls")
+			assert.ElementsMatch(t, mock.zones, mock.zonesCalled, "ListRecommendations was called for different zones")
+		}
+	}
+}
+
+type ErrorZonesService struct {
+	GoogleService
+	err error
+}
+
+func (s *ErrorZonesService) ListZonesNames(project string) ([]string, error) {
+	return []string{}, s.err
+}
+
+func TestErrorInListZones(t *testing.T) {
+	errorMessage := "error listing zones"
+	_, err := ListRecommendations(&ErrorZonesService{err: fmt.Errorf(errorMessage)}, "", "", 2)
+	assert.EqualError(t, err, errorMessage, "Expected error calling ListZones")
+}
+
+type ErrorRecommendationService struct {
+	GoogleService
+	err       error
+	errorZone string
+	mutex     sync.Mutex
+	zones     []string
+}
+
+func (s *ErrorRecommendationService) ListZonesNames(project string) ([]string, error) {
+	return s.zones, nil
+}
+
+func (s *ErrorRecommendationService) ListRecommendations(project string, location string, recommenderID string) ([]*gcloudRecommendation, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if location == s.errorZone {
+		return []*gcloudRecommendation{}, s.err
+	}
+	return []*gcloudRecommendation{}, nil
+}
+
+func TestErrorInRecommendations(t *testing.T) {
+	errorMessage := "error listing recommendations"
+	zones := []string{}
+	for i := 1; i < 5; i++ {
+		zones = append(zones, fmt.Sprintf("zone %d", i))
+	}
+
+	for _, zone := range zones {
+		for numConcurrentCalls := 1; numConcurrentCalls <= 10; numConcurrentCalls++ {
+			_, err := ListRecommendations(
+				&ErrorRecommendationService{
+					err:       fmt.Errorf(errorMessage),
+					zones:     zones,
+					errorZone: zone}, "", "", numConcurrentCalls)
+			assert.EqualError(t, err, errorMessage, "Expected error calling ListRecommendations")
+		}
 	}
 }
 
@@ -46,17 +119,17 @@ type BenchmarkService struct {
 	GoogleService
 }
 
-func (s *BenchmarkService) ListRecommendations(project string, location string, recommenderID string) []*recommender.GoogleCloudRecommenderV1Recommendation {
-	time.Sleep(time.Second)
-	return []*recommender.GoogleCloudRecommenderV1Recommendation{}
+func (s *BenchmarkService) ListRecommendations(project string, location string, recommenderID string) ([]*gcloudRecommendation, error) {
+	time.Sleep(time.Millisecond * 100)
+	return []*gcloudRecommendation{}, nil
 }
 
-func (s *BenchmarkService) ListZonesNames(project string) []string {
+func (s *BenchmarkService) ListZonesNames(project string) ([]string, error) {
 	zones := []string{}
 	for i := 0; i < 100; i++ {
 		zones = append(zones, fmt.Sprintf("zone %d", i))
 	}
-	return zones
+	return zones, nil
 }
 
 func BenchmarkGoroutines(b *testing.B) {
