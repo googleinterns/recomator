403a71d4479f4665b1b30b8f21191036bdc6a261
aziarko <aziarko@google.com>
2020-07-17 17:43:19 +0200

    Added a table for the recommendations

--- frontend/src/views/Mock.vue
+++ frontend/src/views/Mock.vue
@@ -16,3 +16,51 @@ export default {
   }
 };
 </script>
+<template>
+  <div class="mock">
+    <h1>Recomator</h1>
+
+    <v-simple-table>
+      <thead>
+        <tr>
+          <th v-for="header in headers1" v-bind:key="header.value"><span> {{header.text}} </span></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(recommendation, index) in recommendations1" v-bind:key="index">
+            <td> {{recommendation.type}} </td>
+            <td> {{recommendation.cost}} </td>
+            <td> {{recommendation.path}} </td>
+            <td> <v-btn rounded color="primary" :disabled="recommendation.status == applicable" dark x-small>Apply Recommendation</v-btn> </td>
+            <td> {{recommendation.status}} </td> 
+        </tr>
+      </tbody>
+    </v-simple-table>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Mock extends Vue {
+  private headers1 = [
+    { text: "Type", align: "start", sortable: false, value: "type" },
+    { text: "Cost", value: "cost" },
+    { text: "VM path", value: "path" },
+    { text: "Apply", value: "apply" },
+    { text: "Status", value: "status" },
+  ];
+  private recommendations1 = [
+    {
+      type: "xxx",
+      cost: "xxx",
+      path: "xxx",
+      status: "applicable",
+    },
+  ];
+  private headers2 = [];
+  private recommendations2 = [];
+  private name = "Mock";
+}
+</script>
