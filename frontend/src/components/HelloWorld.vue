7b536505bb9a39eaedb550e1218f0c106aa56d9f
shcheshnyak <shcheshnyak@google.com>
2020-07-17 15:21:27 +0100

    Moved the Vuetify quickstart to Home.vue; reverted to routing from the clean Vue quickstart which was overriden by Vuetify

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,15 +1,6 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to Vuetify
