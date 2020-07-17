068b208b2b15977edf4c4915290bcc7781ea86ee
aziarko <aziarko@google.com>
2020-07-17 15:19:33 +0200

    Added a view of a mock page, routing started to work

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -18,6 +18,15 @@ const routes: Array<RouteConfig> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/mock",
+    name: "Mock",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Mock.vue")
   }
 ];
 
