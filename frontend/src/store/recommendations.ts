7d98aaf6d00b13aebc401a83e3c4ba9094240f0f
shcheshnyak <shcheshnyak@google.com>
2020-07-22 14:23:24 +0100

    Moved the data model(model.ts) to src/store/

--- frontend/src/store/recommendations.ts
+++ frontend/src/store/recommendations.ts
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-import "@/data-model/model.ts";
+import "@/store/model";
 
 import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators";
 
--- frontend/tests/unit/example.spec.ts
+++ frontend/tests/unit/example.spec.ts
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-import "@/data-model/model.ts";
+import "@/store/model";
 import RecommendationStore from "@/store/recommendations";
 import Vue from "vue";
 import Vuex from "vuex";
