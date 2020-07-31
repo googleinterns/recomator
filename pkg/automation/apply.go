c363ba4a8d4cbeb64aa1a972a46d076eb08b856a
Viktoriia Kovalova <vkovalova@google.com>
2020-07-31 14:59:05 +0300

    add draft of applying application

---
+++ pkg/automation/apply.go
@@ -0,0 +1,87 @@
+/*
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package automation
+
+import (
+	"strings"
+
+	"google.golang.org/api/recommender/v1"
+)
+
+// DoOperation does the action specified in the operation.
+func (s *googleService) DoOperation(operation *recommender.GoogleCloudRecommenderV1Operation) error {
+	switch strings.ToLower(operation.Action) {
+	case "test":
+		switch operation.Path {
+		case "/machineType":
+			// check machine type
+		case "/status":
+			// check status
+		default:
+			// return error operation not supported
+		}
+	case "replace":
+		switch operation.Path {
+		case "/machineType":
+			// set machine type
+		case "/status":
+			// stop (or start?) the machine
+		default:
+			// return error operation not supported
+		}
+	case "add":
+		switch operation.ResourceType {
+		case "compute.googleapis.com/Snapshot":
+			// create snapshot
+		default:
+			//
+		}
+
+	case "remove":
+		switch operation.ResourceType {
+		case "compute.googleapis.com/Disk":
+			// delete disk
+		default:
+			//
+		}
+
+	default:
+		// return error operation not supported
+	}
+	return nil
+}
+
+// Apply is the method used to apply recommendations from Recommender API.
+// Supports recommendations from the following recommenders:
+// - google.compute.disk.IdleResourceRecommender
+// - google.compute.instance.IdleResourceRecommender
+// - google.compute.instance.MachineTypeRecommender
+func (s *googleService) Apply(recommendation *gcloudRecommendation) error {
+	// check the state is ACTIVE
+	// claim the recommendation
+	for _, operationGroup := range recommendation.Content.OperationGroups {
+		for _, operation := range operationGroup.Operations {
+			err := s.DoOperation(operation)
+			if err != nil {
+				// mark failed
+				return err
+			}
+		}
+	}
+	// mark succedeed
+	return nil
+}
