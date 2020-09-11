cfc85bc3a4b4ab729e749db313898ab09df8d66c
Stanisław Szcześniak <stas.szczesniak@gmail.com>
2020-09-11 17:14:49 +0100

    Linter auto-fix

--- frontend/tests/unit/footer.spec.ts
+++ frontend/tests/unit/footer.spec.ts
@@ -11,7 +11,7 @@ import {
 import vuetify from "@/plugins/vuetify";
 import { Store } from "vuex";
 
-// We want all fetches to be mocked to do nothing here, 
+// We want all fetches to be mocked to do nothing here,
 //  so that tests do not depenend on network requests
 import { enableFetchMocks } from "jest-fetch-mock";
 enableFetchMocks();
